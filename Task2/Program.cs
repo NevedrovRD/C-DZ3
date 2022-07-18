5454bacc39a1e4e317eb00b065b12df313615517
Roman <ramec154@mail.ru>
2022-07-18 03:17:38 +0300

    ДЗ3

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите точку x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите точку y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите точку z1");
+int z1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите точку x2");
+int x2 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите точку y2");
+int y2 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите точку z2");
+int z2 = Convert.ToInt32(Console.ReadLine()); 
+
+double distance = Math.Sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1)));
+
+Console.WriteLine("Расстояние между точками равно " + distance);
---
+++ Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿//Доп. задача
+
+
+Console.WriteLine("Введите число");
+string n = Console.ReadLine();
+int[] x = new int[n.Length];
+for(int i = 0; i < n.Length; i++)
+{
+    x[i] = int.Parse(n[i].ToString());
+}
+
+int index = 0;
+int find = 0;
+
+while (x[index] != 0)
+{
+    index++;
+}
+find = index;
+
+int[] y = new int[find];
+for(int i = 0; i < find; i++)
+{
+    y[i] = int.Parse(x[i].ToString());
+}
+
+int index1 = 0;
+int max = 0;
+int max1 = 0;
+while(index1 < y.Length)
+{
+    if(y[index1] > max)
+    {
+        max1 = max;
+        max = y[index1];
+    }
+        
+index1++;
+}
+    
+Console.WriteLine(max);
+Console.WriteLine(max1);
+
